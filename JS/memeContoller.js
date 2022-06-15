67ac25e024468026e102bf970e8c8b42d84402b9
ynonchayun <ynonchayun@gmail.com>
2022-06-15 14:18:36 +0300

    third upload

--- JS/memeContoller.js
+++ JS/memeContoller.js
@@ -3,18 +3,30 @@ var gElGallery
 var gElEditor
 var gElCanvas
 var gCtx
-
+var imgs
 function onInit() {
     gElGallery = document.querySelector('.gallery-container')
     gElEditor = document.querySelector('.editor-contaioner')
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
+    console.log('bibi');
+    renderImgs()
+}
 
+function renderImgs() {
+    imgs = getImgs()
+    var strHTML = ''
+    imgs.forEach(img => strHTML += `<img src=${imgs[i].url} alt="">`)
+    document.querySelector('.gallery-imgs').innerHTML = strHTML
 }
 
 function renderMeme(elMeme) {
     gElGallery.style.display = 'none'
     gElEditor.style.display = 'flex'
 
-    gCtx.drawImage(elMeme, 0, 0, gElCanvas.width, gElCanvas.height);
+    gCtx.drawImage(getMeme(), 0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+function onSetMeme(meme) {
+    setMeme(meme)
 }
--- JS/memeService.js
+++ JS/memeService.js
@@ -1,11 +1,41 @@
 'use strict'
 
-var gMeme
 
-function getMeme(){
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+var gImgs = [{ id: 1, url: 'img/1.jpg', keywords: ['funny', 'cat'] }]
+var gMeme = {
+    selectedImgId: 5,
+    selectedLineIdx: 0,
+    lines: [{
+        txt: 'I sometimes eat Falafel',
+        size: 20,
+        align: 'left',
+        color: 'red'
+    }
+    ]
+}
+
+function getImgs() {
+    return createImgs()
+};
+
+function createImgs() {
+    for (var i = 1; i < 19; 1++) {
+        gImgs.push({
+            id: i,
+            url: `meme-imgs (square)\ ${i}.jpg`,
+            keywords: ['funny', 'cat']
+        })
+    }
+    return gImgs
+}
+
+function getMeme() {
     return gMeme
 }
 
-function setMeme(meme){
-     gMeme = meme
+function setMeme(meme) {
+    gMeme = meme
+    renderMeme()
 }
