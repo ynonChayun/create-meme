f3e70ff0b3f6ea67cc80c2095db771fabb050c1c
ynonchayun <ynonchayun@gmail.com>
2022-06-17 13:13:42 +0300

    upload

--- JS/controller/listeners.js
+++ JS/controller/listeners.js
@@ -1,165 +1,165 @@
-// var gElCanvas
-// var gCtx
-// var gStartPos
-// const gTouchEvs = ['touchstart', 'touchmove', 'touchend']
+'use stricr'
+var gStartPos
+const gTouchEvs = ['touchstart', 'touchmove', 'touchend']
+var gElCanvas
+var gCtx
+function onInit() {
+    console.log('bibi');
+    gElCanvas = document.querySelector('main-canvas')
+    gCtx = gElCanvas.getContext('2d')
 
-// function onInit() {
-//     gElCanvas = document.querySelector('canvas')
-//     gCtx = gElCanvas.getContext('2d')
+    //Calc the center of the canvas
+    const center = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
+    //Create the circle in the center
+    createCircle(center)
+    addListeners()
+    renderCanvas()
+}
 
-//     resizeCanvas()
-//     //Calc the center of the canvas
-//     const center = { x: gElCanvas.width / 2, y: gElCanvas.height / 2 }
-//     //Create the circle in the center
-//     createCircle(center)
-//     addListeners()
-//     renderCanvas()
-// }
+function renderCanvas() {
+    //Set the backgournd color to grey 
+    gCtx.fillStyle = "#ede5ff"
+    //Clear the canvas,  fill it with grey background
+    gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+    renderCircle()
+}
 
-// function renderCanvas() {
-//     //Set the backgournd color to grey 
-//     gCtx.fillStyle = "#ede5ff"
-//     //Clear the canvas,  fill it with grey background
-//     gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
-//     renderCircle()
-// }
+function renderCircle() {
+    //Get the props we need from the circle 
+    const { pos, color, size } = getCircle()
+    //Draw the circle
+    drawArc(pos.x, pos.y, size, color)
+}
 
-// function renderCircle() {
-//     //Get the props we need from the circle 
-//     const { pos, color, size } = getCircle()
-//     //Draw the circle
-//     drawArc(pos.x, pos.y, size, color)
-// }
+//Handle the listeners
+function addListeners() {
+    addMouseListeners()
+    addTouchListeners()
+    //Listen for resize ev 
+    window.addEventListener('resize', () => {
+        resizeCanvas()
+        renderCanvas()
+    })
+}
 
-// //Handle the listeners
-// function addListeners() {
-//     addMouseListeners()
-//     addTouchListeners()
-//     //Listen for resize ev 
-//     window.addEventListener('resize', () => {
-//         resizeCanvas()
-//         renderCanvas()
-//     })
-// }
+function addMouseListeners() {
+    gElCanvas.addEventListener('mousemove', onMove)
+    gElCanvas.addEventListener('mousedown', onDown)
+    gElCanvas.addEventListener('mouseup', onUp)
+}
 
-// function addMouseListeners() {
-//     gElCanvas.addEventListener('mousemove', onMove)
-//     gElCanvas.addEventListener('mousedown', onDown)
-//     gElCanvas.addEventListener('mouseup', onUp)
-// }
+function addTouchListeners() {
+    gElCanvas.addEventListener('touchmove', onMove)
+    gElCanvas.addEventListener('touchstart', onDown)
+    gElCanvas.addEventListener('touchend', onUp)
+}
 
-// function addTouchListeners() {
-//     gElCanvas.addEventListener('touchmove', onMove)
-//     gElCanvas.addEventListener('touchstart', onDown)
-//     gElCanvas.addEventListener('touchend', onUp)
-// }
+function onDown(ev) {
+    //Get the ev pos from mouse or touch
+    const pos = getEvPos(ev)
+    if (!isCircleClicked(pos)) return
+    setCircleDrag(true)
+    //Save the pos we start from 
+    gStartPos = pos
+    document.body.style.cursor = 'grabbing'
 
-// function onDown(ev) {
-//     //Get the ev pos from mouse or touch
-//     const pos = getEvPos(ev)
-//     if (!isCircleClicked(pos)) return
-//     setCircleDrag(true)
-//     //Save the pos we start from 
-//     gStartPos = pos
-//     document.body.style.cursor = 'grabbing'
+}
 
-// }
+function onMove(ev) {
+    const circle = getCircle();
+    if (circle.isDrag) {
+        const pos = getEvPos(ev)
+        //Calc the delta , the diff we moved
+        const dx = pos.x - gStartPos.x
+        const dy = pos.y - gStartPos.y
+        moveCircle(dx, dy)
+        //Save the last pos , we remember where we`ve been and move accordingly
+        gStartPos = pos
+        //The canvas is render again after every move
+        renderCanvas()
+    }
+}
 
-// function onMove(ev) {
-//     const circle = getCircle();
-//     if (circle.isDrag) {
-//         const pos = getEvPos(ev)
-//         //Calc the delta , the diff we moved
-//         const dx = pos.x - gStartPos.x
-//         const dy = pos.y - gStartPos.y
-//         moveCircle(dx, dy)
-//         //Save the last pos , we remember where we`ve been and move accordingly
-//         gStartPos = pos
-//         //The canvas is render again after every move
-//         renderCanvas()
-//     }
-// }
+function onUp() {
+    setCircleDrag(false)
+    document.body.style.cursor = 'grab'
+}
 
-// function onUp() {
-//     setCircleDrag(false)
-//     document.body.style.cursor = 'grab'
-// }
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+    gElCanvas.width = elContainer.offsetWidth
+    gElCanvas.height = elContainer.offsetHeight
+}
 
-// function resizeCanvas() {
-//     const elContainer = document.querySelector('.canvas-container')
-//     gElCanvas.width = elContainer.offsetWidth
-//     gElCanvas.height = elContainer.offsetHeight
-// }
+function getEvPos(ev) {
 
-// function getEvPos(ev) {
+    //Gets the offset pos , the default pos
+    var pos = {
+        x: ev.offsetX,
+        y: ev.offsetY
+    }
+    // Check if its a touch ev
+    if (gTouchEvs.includes(ev.type)) {
+        //soo we will not trigger the mouse ev
+        ev.preventDefault()
+        //Gets the first touch point
+        ev = ev.changedTouches[0]
+        //Calc the right pos according to the touch screen
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
+        }
+    }
+    return pos
+}
 
-//     //Gets the offset pos , the default pos
-//     var pos = {
-//         x: ev.offsetX,
-//         y: ev.offsetY
-//     }
-//     // Check if its a touch ev
-//     if (gTouchEvs.includes(ev.type)) {
-//         //soo we will not trigger the mouse ev
-//         ev.preventDefault()
-//         //Gets the first touch point
-//         ev = ev.changedTouches[0]
-//         //Calc the right pos according to the touch screen
-//         pos = {
-//             x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
-//             y: ev.pageY - ev.target.offsetTop - ev.target.clientTop
-//         }
-//     }
-//     return pos
-// }
-
-// function drawArc(x, y, size = 60, color = 'blue') {
-//     gCtx.beginPath()
-//     gCtx.lineWidth = '6'
-//     gCtx.arc(x, y, size, 0, 2 * Math.PI)
-//     gCtx.strokeStyle = 'white'
-//     gCtx.stroke()
-//     gCtx.fillStyle = color
-//     gCtx.fill()
-// }
-
-
-// // //////////////////////////////////////////
-// var gCircle
-
-// function createCircle(pos) {
-//     gCircle = {
-//         pos,
-//         size: 60,
-//         color: 'blue',
-//         isDrag: false
-//     }
-// }
-
-// function getCircle() {
-//     return gCircle
-// }
-
-// //Check if the click is inside the circle 
-// function isCircleClicked(clickedPos) {
-//     const { pos } = gCircle
-//     // Calc the distance between two dots
-//     const distance = Math.sqrt((pos.x - clickedPos.x) ** 2 + (pos.y - clickedPos.y) ** 2)
-//     //If its smaller then the radius of the circle we are inside
-//     return distance <= gCircle.size
-// }
+function drawArc(x, y, size = 60, color = 'blue') {
+    gCtx.beginPath()
+    gCtx.lineWidth = '6'
+    gCtx.arc(x, y, size, 0, 2 * Math.PI)
+    gCtx.strokeStyle = 'white'
+    gCtx.stroke()
+    gCtx.fillStyle = color
+    gCtx.fill()
+}
 
 
-// function setCircleDrag(isDrag) {
-//     gCircle.isDrag = isDrag
-// }
+// //////////////////////////////////////////
+var gCircle
 
-// //Move the circle in a delta, diff from the pervious pos
-// function moveCircle(dx, dy) {
-//     gCircle.pos.x += dx
-//     gCircle.pos.y += dy
+function createCircle(pos) {
+    gCircle = {
+        pos,
+        size: 60,
+        color: 'blue',
+        isDrag: false
+    }
+}
 
-// }
+function getCircle() {
+    return gCircle
+}
+
+//Check if the click is inside the circle 
+function isCircleClicked(clickedPos) {
+    const { pos } = gCircle
+    // Calc the distance between two dots
+    const distance = Math.sqrt((pos.x - clickedPos.x) ** 2 + (pos.y - clickedPos.y) ** 2)
+    //If its smaller then the radius of the circle we are inside
+    return distance <= gCircle.size
+}
+
+
+function setCircleDrag(isDrag) {
+    gCircle.isDrag = isDrag
+}
+
+//Move the circle in a delta, diff from the pervious pos
+function moveCircle(dx, dy) {
+    gCircle.pos.x += dx
+    gCircle.pos.y += dy
+
+}
 
 
 
